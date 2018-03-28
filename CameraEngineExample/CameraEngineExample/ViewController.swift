771fa8ab94b6a3583a66288a1762093aba3d7080
Arjay Angeles <aqangeles@gmail.com>
2018-03-28 13:42:26 +0800

    Add pinch to zoom feature.

--- CameraEngineExample/CameraEngineExample/ViewController.swift
+++ CameraEngineExample/CameraEngineExample/ViewController.swift
@@ -15,32 +15,35 @@ enum ModeCapture {
 }
 
 class ViewController: UIViewController {
-    
+
     private var cameraEngine = CameraEngine()
     private var mode: ModeCapture = .Photo
-    
+
+    var vZoomFactor: CGFloat = 1.0
+
     @IBOutlet weak var buttonMode: UIButton!
     @IBOutlet weak var labelMode: UILabel!
     @IBOutlet weak var buttonTrigger: UIButton!
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         guard let layer = self.cameraEngine.previewLayer else {
             return
         }
         layer.frame = self.view.bounds
         self.view.layer.insertSublayer(layer, at: 0)
+        self.view.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(pinchToZoom(pinch:))))
     }
-    
+
     @IBAction func setModeCapture(_ sender: AnyObject) {
         let alertController = UIAlertController(title: "set mode capture", message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Photo", style: .default, handler: {  _ in
+        alertController.addAction(UIAlertAction(title: "Photo", style: .default, handler: { _ in
             self.labelMode.text = "Photo"
             self.buttonTrigger.setTitle("take picture", for: .normal)
             self.mode = .Photo
         }))
-        alertController.addAction(UIAlertAction(title: "Video", style: .default, handler: {  _ in
+        alertController.addAction(UIAlertAction(title: "Video", style: .default, handler: { _ in
             self.labelMode.text = "Video"
             self.buttonTrigger.setTitle("start recording", for: .normal)
             self.mode = .Video
@@ -48,10 +51,10 @@ class ViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
-    
+
     @IBAction func encoderSettingsPresset(_ sender: AnyObject) {
         let alertController = UIAlertController(title: "Encoder settings", message: nil, preferredStyle: .actionSheet)
-        
+
         alertController.addAction(UIAlertAction(title: "Preset640x480", style: .default, handler: { _ in
             self.cameraEngine.videoEncoderPresset = .Preset640x480
         }))
@@ -67,14 +70,14 @@ class ViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Preset3840x2160", style: .default, handler: { _ in
             self.cameraEngine.videoEncoderPresset = .Preset3840x2160
         }))
-        
+
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
-    
+
     @IBAction func setZoomCamera(_ sender: AnyObject) {
         let alertController = UIAlertController(title: "set zoom factor", message: nil, preferredStyle: .actionSheet)
-        
+
         alertController.addAction(UIAlertAction(title: "X1", style: .default, handler: { _ in
             self.cameraEngine.cameraZoomFactor = 1
         }))
@@ -90,14 +93,37 @@ class ViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "X5", style: .default, handler: { _ in
             self.cameraEngine.cameraZoomFactor = 5
         }))
-        
+
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
-    
+
+    @objc func pinchToZoom(pinch: UIPinchGestureRecognizer) {
+        var device = self.cameraEngine.captureDevice
+        vZoomFactor = pinch.scale
+        var error: NSError!
+        do {
+            try device?.lockForConfiguration()
+            defer { device?.unlockForConfiguration() }
+            if (vZoomFactor <= (device?.activeFormat.videoMaxZoomFactor)!) {
+                if vZoomFactor < 1.0 {
+                    vZoomFactor = 1.0
+                }
+                device?.videoZoomFactor = vZoomFactor
+            } else {
+                NSLog("Unable to set videoZoom: (max %f, asked %f)", device?.activeFormat.videoMaxZoomFactor ?? 1, vZoomFactor)
+            }
+        } catch error as NSError {
+            NSLog("Unable to set videoZoom: %@", error.localizedDescription)
+        } catch _ {
+
+        }
+    }
+
+
     @IBAction func setFocus(_ sender: AnyObject) {
         let alertController = UIAlertController(title: "set focus settings", message: nil, preferredStyle: .actionSheet)
-        
+
         alertController.addAction(UIAlertAction(title: "Locked", style: .default, handler: { _ in
             self.cameraEngine.cameraFocus = CameraEngineCameraFocus.locked
         }))
@@ -110,19 +136,19 @@ class ViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
     }
-    
+
     @IBAction func switchCamera(_ sender: AnyObject) {
         self.cameraEngine.switchCurrentDevice()
     }
-    
+
     @IBAction func takePhoto(_ sender: AnyObject) {
         switch self.mode {
         case .Photo:
-            self.cameraEngine.capturePhoto { (image , error) -> (Void) in
+            self.cameraEngine.capturePhoto { (image, error) -> (Void) in
                 if let image = image {
                     CameraEngineFileManager.savePhoto(image, blockCompletion: { (success, error) -> (Void) in
                         if success {
-                            let alertController =  UIAlertController(title: "Success, image saved !", message: nil, preferredStyle: .alert)
+                            let alertController = UIAlertController(title: "Success, image saved !", message: nil, preferredStyle: .alert)
                             alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
                             self.present(alertController, animated: true, completion: nil)
                         }
@@ -139,7 +165,7 @@ class ViewController: UIViewController {
                                 self.buttonTrigger.setTitle("start recording", for: .normal)
                                 CameraEngineFileManager.saveVideo(url, blockCompletion: { (success: Bool, error: Error?) -> (Void) in
                                     if success {
-                                        let alertController =  UIAlertController(title: "Success, video saved !", message: nil, preferredStyle: .alert)
+                                        let alertController = UIAlertController(title: "Success, video saved !", message: nil, preferredStyle: .alert)
                                         alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
                                         self.present(alertController, animated: true, completion: nil)
                                     }
@@ -158,7 +184,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         self.cameraEngine.rotationCamera = true
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cameraEngine.startSession()
